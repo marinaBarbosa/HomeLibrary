3e757be07972e1d945423790bdbf660afe9498cf
Marina Barbosa <marina.veiga.barbosa@gmail.com>
2017-03-16 17:32:26 +0000

    [1]Connection MongoDB

---
+++ api/models/User.js
@@ -0,0 +1,26 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	name: {
+  		type: 'string',
+  		required: true,
+  	},
+  	email: {
+  		type: 'string',
+  		required: true,
+  		unique: true
+  	},
+  	password: {
+  		type: 'string',
+  		required: true
+  	}
+  }
+};
+
