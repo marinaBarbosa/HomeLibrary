99225889928c6b44ff7280c5bcdcd873d8d72c5c
Marina Barbosa <marina.veiga.barbosa@gmail.com>
2017-03-27 11:26:01 +0100

    [3] Some updates

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,25 @@
  */
 
 module.exports = {
-	
+		'new': function (req,res) {
+			res.view();
+	},
+
+	create: function (req, res, next) {
+		User.create (req.params.all(), function userCreated (err, user) {
+			if (err) return (err);
+
+			res.redirect('/user/show/' + user.id);
+		});
+	},
+	show: function(req, res, next) {
+		User.findOne(req.param('id'), function foundUser (err, user) {
+			if (err) return next (err);
+			if (!user) return next();
+			res.view({
+				user: user
+			});
+		});
+	}
 };
 
